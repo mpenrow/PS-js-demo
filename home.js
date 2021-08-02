2f46d54dffa248c9c71f9bd1629aea39719aae3c
Michael Penrow <106648+mpenrow@users.noreply.github.com>
2021-08-02 18:53:25 -0500

    Finished Object Properties section.

--- home.js
+++ home.js
@@ -1,1 +1,12 @@
-changePercentOff(32);
+let mySymbol = Symbol();
+
+let person = {
+    name: 'John',
+    age: 32,
+    partTime: false,
+    [mySymbol]: 'secretInformation'
+};
+
+person['age'] = 44;
+
+showMessage(person.age);
