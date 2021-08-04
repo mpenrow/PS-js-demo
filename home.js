6329c9491135c612bacd6edc733b9207682d9e62
Michael Penrow <106648+mpenrow@users.noreply.github.com>
2021-08-04 06:30:11 -0500

    Finished the manipulating arrays section.

--- home.js
+++ home.js
@@ -1,16 +1,3 @@
-const button = document.getElementById("see-review");
-
-button.addEventListener('click', function() {
-    const review = document.getElementById("review");
-
-    const dNone = 'd-none';
-    if (review.classList.contains(dNone)) {
-        review.classList.remove(dNone);
-        button.textContent = "CLOSE REVIEW";
-    }
-    else {
-        review.classList.add(dNone);
-        button.textContent = "SEE REVIEW";
-    }
-    
-});
+const values = ["a", "b", "c"];
+values.unshift('hello', 'world');
+console.log(values);
